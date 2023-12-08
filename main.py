fbb36accb5970b3c920207e83ead52abb61130b8
Voievodenko <148055725+Voievodenko@users.noreply.github.com>
2023-12-08 15:30:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-file_txt= 'text.txt'
+name = 'text.txt'
 
 
-def change(file, word1, word2)
-  with open(file, 'r', encoding='utf-8') as func:
-    text = func.read()
+def change(file, word1, word2):
+    with open(file, 'r', encoding='utf-8') as func:
+        text = func.read()
 
-  update=text.replace(word1, word2)
+    update = text.replace(word1, word2)
 
-  with open(file, 'w', encoding= 'utf-8') as rep:
-    rep.write(update)
+    with open(file, 'w', encoding='utf-8') as rep:
+        rep.write(update)
 
 
-word1 = input("Введіть слово яке хочете змінити: ")
-word2 = input("Введіть слово на яке хочете змінити: ")
-change(file_txt, word1, word2)
+word1 = input("Введіть слово для заміни: ")
+word2 = input("Введіть слово, на яке потрібно замінити: ")
+
+change(name, word1, word2)
